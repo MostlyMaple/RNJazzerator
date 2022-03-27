ac18077d006c8e98ea70092b2ccb2f7538a796f3
matthewsander3 <matthewsander3@gmail.com>
2022-03-26 20:41:16 -0500

    Stage 3 html update, colors

--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, print_function, unicode_literals
+from wolframclient.evaluation import WolframCloudSession
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import AudioForm
@@ -6,6 +8,14 @@ import wave
 import struct
 
 # Create your views here.
+def call(x):
+    """ Call the API using function input parameter values.
+    If the API was deployed with an export formats set to JSON or WXF, the result is often a native Python type.
+    """
+    with WolframCloudSession() as session:
+        api_response = session.call('https://www.wolframcloud.com/obj/9455e160-850b-499f-97ed-a2fd924e814d', {'x' : x})
+        return api_response.get()
+
 def home(request):
     context = {}
     return render(request, 'home.html', context)
